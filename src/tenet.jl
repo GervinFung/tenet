1cb39e8e1e82095c68330b68ea9765190ce80294
unknown <rongsoon2000@gmail.com>
2021-08-28 02:33:26 +0800

    Multiplication

--- src/tenet.jl
+++ src/tenet.jl
@@ -2,11 +2,8 @@ module tenet
 
 export add_num
 export get_tenet
-<<<<<<< Updated upstream
 export gcd_euclid_algorithm
-=======
 export multi_num
->>>>>>> Stashed changes
 
 include("add.jl")
 include("string.jl")
