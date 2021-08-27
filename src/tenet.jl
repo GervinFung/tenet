b9833d7569799ca5f307e490e843e0edbd25c0c9
DrakonDarquesse <mingliangzheng17@gmail.com>
2021-08-28 02:45:23 +0800

    export divide

--- src/tenet.jl
+++ src/tenet.jl
@@ -3,9 +3,11 @@ module tenet
 export add_num
 export get_tenet
 export gcd_euclid_algorithm
+export divide_num
 
 include("add.jl")
 include("string.jl")
 include("gcd.jl")
+include("divide.jl")
 
 end
